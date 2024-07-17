56d5ce56e14749e99cd47aa062be2baa7b1e2fff
DJ Hemath <djhemath@gmail.com>
2024-07-17 10:29:22 +0530

    feat: route config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import Auth from './pages/Auth';
+import Home from './pages/Home';
+import PrivateRoute from './components/PrivateRoute';
+
 
 function App() {
   return (
-    <>
-      <h1>Devprofile Page</h1>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route element={<PrivateRoute />}>
+          <Route path='/' Component={Home} />
+        </Route>
+        <Route path='/login' Component={Auth} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
